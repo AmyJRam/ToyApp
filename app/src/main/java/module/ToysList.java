a41f41ad1e18e3d18d77545721f871c1a195a4ee
amy <amruthajayaram19@gmail.com>
2017-10-11 22:32:23 +0530

    After Small changes in MainActivity

---
+++ app/src/main/java/module/ToysList.java
@@ -0,0 +1,17 @@
+package module;
+
+/**
+ * Created by amy on 11/10/17.
+ */
+
+public class ToysList {
+   /*
+   * This method return a list of Toys Name
+   * return Toys String array
+     */
+   public static String[] getToys()
+   {
+       String Toys[]={"Baby rattle","Bamboo-copter","Channapatna toys","Newton's cradle"};
+       return  Toys;
+   }
+}
